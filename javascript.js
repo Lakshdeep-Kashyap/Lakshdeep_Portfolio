2fece9b44d2b7db492cc392e6daf3b4bb71b1c1c
LakshdeepKashyap <anusuman817@gmail.com>
2025-01-31 21:33:51 +0530

    DEPLOY

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// console.log(window);
+// console.dir(window);
+
+// console.log(window.document);
+// console.dir(window.document);
+
+// let el = document.getElementsByTagName("p");
+// let h1 = document.querySelectorAll("p");
+// console.dir(h1);
+// console.dir(el);
+
+// let div = document.querySelector("div");
+// console.log(div);
+
+// div.style.border = "10px solid pink" ;
+
+// el[1].innerText = el[1].innerText + "NIGGGGGGGGA";
+
+// let div = document.querySelectorAll("div");
+// div[0].setAttribute("class","Nigger");
+// div[1].setAttribute("class","Nigger");
+
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+
+btn1.onclick = () => {
+    console.log("Asking for comission");
+};
+
+btn2.ondblclick = () => {
+    console.log("Tell experience and projects in your field");
+};
